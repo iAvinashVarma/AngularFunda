86d0da26416367e74944eece98f22b885be97435
Avinash Varma <avinashvarma@live.com>
2019-05-01 18:53:58 +0800

    Add custom filters directly intead of using $filter service.

--- app/js/controllers/filterSampleCtrl.js
+++ app/js/controllers/filterSampleCtrl.js
@@ -1,12 +1,11 @@
 'use strict'
 
 eventsApp.controller('filterSampleCtrl',
-    function filterSampleCtrl($scope, $filter){
+    function filterSampleCtrl($scope, durationsFilter){
         $scope.data = {};
-        var durations = $filter('durations');
-        $scope.data.duration1 = durations(1);
-        $scope.data.duration2 = durations(2);
-        $scope.data.duration3 = durations(3);
-        $scope.data.duration4 = durations(4);
+        $scope.data.duration1 = durationsFilter(1);
+        $scope.data.duration2 = durationsFilter(2);
+        $scope.data.duration3 = durationsFilter(3);
+        $scope.data.duration4 = durationsFilter(4);
     }
 );
