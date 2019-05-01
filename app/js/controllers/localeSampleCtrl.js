5833e93650e0d946abdf74fb93c9dd4d06e8e82d
Avinash Varma <avinashvarma@live.com>
2019-05-01 16:01:04 +0800

    Add $locale log to check the properties.

--- app/js/controllers/localeSampleCtrl.js
+++ app/js/controllers/localeSampleCtrl.js
@@ -1,7 +1,8 @@
 'use strict'
 
 eventsApp.controller('localeSampleCtrl',
-    function localeSampleCtrl($scope, $locale){
+    function localeSampleCtrl($scope, $locale, $log){
+        $log.info($locale);
         $scope.myDate = Date.now();
         $scope.fullDateFormat = $locale.DATETIME_FORMATS.fullDate;
         $scope.shortDateFormat = $locale.DATETIME_FORMATS.shortDate;
